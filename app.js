517b604c47ff6a721f151a877a01d8d6cef4f80e
KyorCode <t-c@live.be>
2013-08-26 23:12:14 +0200

    Structure Node Js files

--- app.js
+++ app.js
@@ -7,57 +7,13 @@ var express = require('express'),
     pub = __dirname + '/public',
     doT = require('express-dot');
 
-function logErrors(err, req, res, next) {
-    console.error(err.stack);
-    next(err);
-}
 
-function clientErrorHandler(err, req, res, next) {
-    if (req.xhr) {
-        res.send(500, {error: 'You killed it!'});
-    }
-    else {
-        next(err);
-    }
-}
-
-function errorHandler(err, req, res, next) {
-    res.status(500);
-    res.render('error', {error: err});
-}
-
-
-app.configure('development', function () {
-    app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
-});
-
-app.configure('production', function () {
-    app.use(express.errorHandler());
-});
-
-io.configure(function () {
-    io.set("transports", ["xhr-polling"]);
-    io.set("polling duration", 10);
-});
+require('./lib/environment.js')(app, express, {'view': view, 'doT': doT, 'pub': pub});
+require('./lib/routes.js')(app);
+require('./lib/socket.js')(io);
 
 var port = process.env.PORT || 35728;
 server.listen(port, function () {
     console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
 });
 
-app.get('/', function (req, res) {
-    var templateData = {title: "test"};
-    res.render('index.html', templateData);
-});
-
-
-app.set('views', view);
-app.set('view engine', 'doT');
-app.engine('html', doT.__express);
-app.use(express.bodyParser());
-app.locals.layout = false;
-app.use(express.static(pub));
-app.use(logErrors);
-app.use(clientErrorHandler);
-app.use(errorHandler);
-app.use(app.router);
